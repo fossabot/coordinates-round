5f654ea3c78225b0a85506408b75b7a87561cff2
YousafAzabi <joeazabi@gmail.com>
2018-11-26 09:11:50 +0000

    Added code to round coordinates of  MultiLineString

--- src/round-coord.js
+++ src/round-coord.js
@@ -1,15 +1,34 @@
 //module to round coordinates to 7 decimal points
 
+//======== main function to round coordinates of given feature ===========
 exports.roundCoordinates = (feature, digits = 7) => {
-  coordinates = feature.geometry.coordinates;
-  for(let i = 0; i < coordinates.length; i++) { //loop through coordinates of a link
-    coordinates[i][0] = round(coordinates[i][0], digits); //round longitude of i point
-    coordinates[i][1] = round(coordinates[i][1], digits); //round latitude of i point
-  }
+  let coordinates = []; //declare coordinates array as empty
+  let geometry = feature.geometry; //
+  if (geometry.type == 'LineString') {
+    //check geometry type if LineString call loop fn once
+     coordinates = loop(geometry.coordinates, digits);
+  } else if (geometry.type == 'MultiLineString') {
+    //if type MultiLineString call loop fn for each element
+    geometry.coordinates.forEach( coordinate =>
+      coordinates.push(loop(coordinate, digits))
+    );
+  } 
+
   feature.geometry.coordinates = coordinates;
   return feature; //return rounded coordinates
 }
 
-round = (n, d) => { //inner function that rounds numbers
+//====== function to loop through coordinates and call round function  ========
+loop = (coordinates, digits) => {
+  for(let i = 0; i < coordinates.length; i++) { //loop through coordinates of a link
+    coordinates[i][0] = round(coordinates[i][0], digits); //round longitude of i point
+    coordinates[i][1] = round(coordinates[i][1], digits); //round latitude of i point
+  }
+  return coordinates; //return rounded coordinates
+}
+
+
+//================== function to round numbers  ======================
+round = (n, d) => {
   return Math.round( n * Math.pow(10, d) ) / Math.pow(10, d);  //round to d decimal points
 }
--- test/round-coord.js
+++ test/round-coord.js
@@ -8,13 +8,13 @@ const feature = {
     "name": "road name"
   },
   "geometry": {
-    "type": "StringLine",
+    "type": "LineString",
     "coordinates":[]
   }
 }
 
-describe('round-ccord.js to round coordinates to given decimal point', () => {
-  it('Test when coordinates are rounded with some to upper and lower bound.', () => {
+describe('round-ccord.js to round coordinates to given decimal digits', () => {
+  it('Test when coordinates are LineString and rounded with some to upper and lower bound.', () => {
     const input = feature;
     input.geometry.coordinates = [[-0.1349098573, 51.5246098373], [-0.13596983326, 51.52333342]];
     const expected = [[-0.1349099, 51.5246098], [-0.1359698, 51.5233334]];
@@ -22,7 +22,15 @@ describe('round-ccord.js to round coordinates to given decimal point', () => {
     expect(output.geometry.coordinates).to.eql(expected);
   });
 
-  it('Test when coordinates are short and not rounded.', () => {
+  it('Test when coordinates are LineString and rounded to 5 decimal digits.', () => {
+    const input = feature;
+    input.geometry.coordinates = [[-0.1349098573, 51.5246098373], [-0.13596983326, 51.52333342]];
+    const expected = [[-0.13491, 51.52461], [-0.13597, 51.52333]];
+    const output = roundCoordinates(input, 5);
+    expect(output.geometry.coordinates).to.eql(expected);
+  });
+
+  it('Test when coordinates are LineString and short and not rounded.', () => {
     const input = feature;
     input.geometry.coordinates = [[-0.1349, 51.52], [-0.1355678, 51]];
     const expected = [[-0.1349, 51.52], [-0.1355678, 51]];
@@ -30,11 +38,38 @@ describe('round-ccord.js to round coordinates to given decimal point', () => {
     expect(output.geometry.coordinates).to.eql(expected);
   });
 
-  it('Test when coordinates are empty array.', () => {
+  it('Test when coordinates are LineString and empty array.', () => {
     const input = feature;
     input.geometry.coordinates = [];
     const expected = [];
     const output = roundCoordinates(input);
+    expect(output.geometry.coordinates).to.eql(expected);
+  });
+
+  it('Test when coordinates are MultiLineString and rounded with some to upper and lower bound.', () => {
+    const input = feature;
+    input.geometry.type = "MultiLineString";
+    input.geometry.coordinates = [[[-0.1349098573, 51.5246098373], [1, 2]], [-0.135, 51]];
+    const expected = [[[-0.1349099, 51.5246098], [1, 2]], [-0.135, 51]];
+    const output = roundCoordinates(input);
+    expect(output.geometry.coordinates).to.eql(expected);
+  });
+
+  it('Test when coordinates are MultiLineString and rounded to 5 decimal digits.', () => {
+    const input = feature;
+    input.geometry.type = "MultiLineString";
+    input.geometry.coordinates = [[[-0.1349098573, 51.5246098373], [1, 2]], [-0.135, 51]];
+    const expected = [[[-0.13491, 51.52461], [1, 2]], [-0.135, 51]];
+    const output = roundCoordinates(input, 5);
+    expect(output.geometry.coordinates).to.eql(expected);
+  });
+
+  it('Test when coordinates are MultiLineString and empty array.', () => {
+    const input = feature;
+    input.geometry.type = "MultiLineString";
+    input.geometry.coordinates = [];
+    const expected = [];
+    const output = roundCoordinates(input, 5);
     expect(output.geometry.coordinates).to.eql(expected);
   });
 });
